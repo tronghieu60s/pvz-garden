08406a852064adc713026d371ddda874139f8358
Phan Duc <duc.phan622@gmail.com>
2021-08-28 16:04:28 +0700

    add logic when choose plant

--- src/components/SendBank.js
+++ src/components/SendBank.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import SendBackItem from "./SendBankItem";
 
 export default function SendBank(props) {
   const { plants } = props;
+   const [choosePlantKey, setChoosePlantKey] = useState("plant1");
   return (
     <div className="gd-sendBank">
       {plants.map((item) => (
-        <SendBackItem key={item.key} plant={item} />
+        <SendBackItem key={item.key} plant={item} choosePlantKey={choosePlantKey} setChoosePlantKey={setChoosePlantKey} />
       ))}
     </div>
   );
--- src/components/SendBankItem.js
+++ src/components/SendBankItem.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React,{useState} from "react";
 
 export default function sendBankItem(props) {
-  const { plant, active = true } = props;
+  const { plant, active = true, choosePlantKey, setChoosePlantKey } = props;
+ 
   return (
-    <div className={`gd-sendBank-item${active ? " active" : ""}`}>
+    <div
+      className={`gd-sendBank-item${choosePlantKey !== plant.key ? " active" : ""}`}
+      onClick={() => {
+        setChoosePlantKey(plant.key);
+      }}
+    >
       <img src={plant.image1b} alt="" />
       <img src={plant.image2b} alt="" />
       <div className="gd-sendBank-item-point">{plant.point}</div>
