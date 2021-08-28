8a9ac5319f4f9e2d3f5dd2637d7835bd00b5cc7c
Phan Duc <duc.phan622@gmail.com>
2021-08-28 18:51:22 +0700

    fix bug when tree is planted

--- src/components/GardenItem.js
+++ src/components/GardenItem.js
@@ -1,23 +1,37 @@
-import React, { Fragment, useState, useRef } from "react";
+import React, { Fragment, useState, useRef, useEffect } from "react";
 
 export default (props) => {
   const { plant, choosePlant, positionGardenItem, isSelectedPlant } = props;
   const [plantItem, setPlantItem] = useState({
     image: null,
     purchasePrice: null,
+    timer: null,
   });
+  const [isPlanted, setIsPlanted] = useState(false);
   const chooseGardenItem = useRef(null);
+
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     //console.log(choosePlant.purchasePrice);
+  //   }, 3000);
+  // }, [choosePlant]);
 
   return (
     <div
       className="gd-garden-item"
       onClick={() => {
-        if (!isSelectedPlant) alert("Vui lòng chọn câyyyyyyyyyyyyyyyyyyyyyy");
-        chooseGardenItem.current = positionGardenItem;
-        setPlantItem({
-          image2: choosePlant.image2,
-          purchasePrice: choosePlant.purchasePrice,
-        });
+        if (!isSelectedPlant) alert("Vui lòng chọn cây");
+        if (isPlanted) {
+          alert("Cây đã được trồng, vui lòng chọn ô khác");
+        } else {
+          chooseGardenItem.current = positionGardenItem;
+          setPlantItem({
+            image2: choosePlant.image2,
+            purchasePrice: choosePlant.purchasePrice,
+            timer: choosePlant.timer,
+          });
+          setIsPlanted(true);
+        }
       }}
     >
       {plant && (
@@ -25,7 +39,7 @@ export default (props) => {
           <div className="gd-garden-image">
             <img src={plantItem.image2} />
           </div>
-          <div className="gd-garden-item-timer">{plantItem.purchasePrice}</div>
+          <div className="gd-garden-item-timer">111</div>
         </Fragment>
       )}
     </div>
