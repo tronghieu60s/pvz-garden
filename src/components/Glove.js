6acc655e82b05a42225f17b788d1579908a204b2
Phan Duc <duc.phan622@gmail.com>
2021-08-29 00:04:23 +0700

    add logic get glove

--- src/components/Glove.js
+++ src/components/Glove.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default () => {
+  const [isGetGlove, setIsGetGlove] = useState(false);
+
+  const handleGetGlove = () => {
+    setIsGetGlove(!isGetGlove);
+  };
+
   return (
-    <div className="bank">
-      <div className="glove"></div>
+    <div className="bank" onClick={() => handleGetGlove()}>
+      <div className={isGetGlove ? "" : "glove"}></div>
     </div>
   );
 };
--- src/components/SendBank.js
+++ src/components/SendBank.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import SendBackItem from "./SendBankItem";
 
 export default function SendBank(props) {
